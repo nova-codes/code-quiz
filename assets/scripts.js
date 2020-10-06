95560d6f8dce93980a6d40f2ca17d44eb0fcca6d
Hannah Elliott <hgelliott90@gmail.com>
2020-10-05 20:55:28 -0400

    Trying to get answers to display and failing

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,41 +2,39 @@
 var startButton = document.querySelector('.start-btn'),
     startScreen = document.querySelector('#start-screen'),
     quizDescription = document.querySelector('.quiz-start'),
-    questionText = document.querySelector('.question-text'),
+    questionText = document.querySelector('#question-text'),
     answerDisplay = document.querySelector('.answer'),
     timerCount = document.querySelector('.timer-count'),
     scoreCount = document.querySelector('.score-count');
+
+var i = 0; 
 
 var quizQuestions = [
     {
         count: 1,
         question: "Javascript is a _____-side programming language.",
-        answers: {
-            a: 'Client',
-            b: 'Server',
-            c: 'Both',
-            d: 'None'
-        },
-        correct: 'c'
+        answers: [
+            {text: 'Client', correct: false},
+            {text: 'Server', correct: false},
+            {text: 'Both', correct: true},
+            {text: 'None', correct: false}
+        ]
     },
     {
         count: 2,
         question: "Which of the following will write 'Help' in an alert box?",
-        answers: {
-            a: 'alertBox("Help")',
-            b: 'alert(Help)',
-            c: 'alertText("Help")',
-            d: 'alert("Help")'
-        },
-        correct: 'd'
+        answers: [
+            {text: 'alertBox("Help")', correct: false},
+            {text: 'alert(Help)', correct: false},
+            {text: 'alertText("Help")', correct: false},
+            {text: 'alert("Help")', correct: true}
+        ]
     }
 ]
 
 // hide the start screen when button clicked
 
 function hideStart(){
-
-    startButton.addEventListener('click', function(){
         if(startScreen.style.opacity = 1) {
             startScreen.style.opacity = 0;
             startScreen.style.zIndex = -1; 
@@ -44,10 +42,12 @@ function hideStart(){
         else {
             console.log(startScreen.style.opacity); 
         }
-    })
-}
+        timer();
+        displayQuestions();
 
-hideStart(); 
+    };
+
+    startButton.addEventListener('click', hideStart);
 
 // begin timer
 
@@ -61,7 +61,12 @@ function timer() {
     }, 1000); 
 }
 
-timer();
-
 // show questions
+function displayQuestions(questionText) {
+    questionText.textContent = quizQuestions[i].question; 
 
+    var quizAnswers = Object.values(quizQuestions[i].answers[text]);
+
+    answerDisplay.textContent = quizAnswers;
+
+}
